fc10743fcf31c24e361ebe0d767d1ef200270dbc
Genta-hoxha <genta.hoxha10@gmail.com>
2023-09-14 17:10:24 +0200

    day9

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -17,7 +17,7 @@ function first() {
 }
 first();
 */
-
+/*
 const a = 'Jonas';
 first();
 
@@ -33,3 +33,246 @@ function third() {
   const d = 'Hey!';
   console.log(d + c + b + a); //ReferenceERROR
 }
+*/
+
+/*
+function calcAge(birthYear) {
+  //calcAge defined ne nje global scope sepse eshte ne top level code
+  const age = 2023 - birthYear;
+  //   console.log(firstName);
+
+  //how to create function inside function
+  function printAge() {
+    const output = `${firstName}, you are ${age} years old, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      console.log(`Oh, and you are a millenial, ${firstName}`);
+      console.log(str);
+    }
+    console.log(millenial);
+  }
+  printAge();
+  return age;
+}
+
+//create a global variable
+const firstName = 'Genta';
+calcAge(1996);
+//console.log(age);
+//printAge();
+
+*/
+
+/*
+const myName = 'Genta';
+if (myName === 'Genta') {
+  //TDZ for job variable (Pra, ReferenceError: Cannot access 'job' before initialization)
+  console.log(`Genta is a ${job}`);
+  const age = 2023 - 1996;
+  console.log(age);
+
+  const job = 'programmer';
+  console.log(x);   //ReferenceError: x is not defined
+}
+*/
+
+/*----HOISTING AND TDZ in PRACTICE ------*/
+/*
+//Variables
+console.log(me);
+//console.log(job);
+//console.log(year);
+
+var me = 'Genta';
+let job = 'programmer';
+const year = 1996;
+
+//Functions
+console.log(addDecl(2, 3));
+console.log(addExpr(2, 3));
+console.log(addArrow(2, 3));
+function addDecl(a, b) {
+  return a + b;
+}
+const addExpr = function (a, b) {
+  //is in TDZ sepse ka const perpara
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+//Notes: The only function than can uyse before declaring , is the function declaration
+*/
+/*
+//Example
+console.log('undefined');
+if (!numProducts) deleteShoppingCart();
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x); //Outputi: true
+console.log(y === window.y); //Outputi: false
+console.log(z === window.z); //Outputi: false
+*/
+
+/*
+//THIS KEYWORD
+//console.log(this);
+const calcAge = function (birthYear) {
+  console.log(2023 - birthYear);
+  console.log(this);
+};
+calcAge(1996);
+
+const calcAgeArrow = birthYear => {
+  console.log(2023 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1996);
+
+const jonas = {
+  year: 1996,
+  calcAge: function () {
+    console.log(this);
+    console.log(2023 - this.year);
+  },
+};
+jonas.calcAge();
+
+const genta = {
+  year: 1990,
+};
+genta.calcAge = jonas.calcAge;
+genta.calcAge();
+
+// const f = jonas.calcAge;
+// f();
+*/
+
+/*----Regular functions vs.Arrow functions------*/
+/*
+//var firstName = 'Matilda';
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    //console.log(this);
+    console.log(2023 - this.year);
+
+    //METHOD 1
+
+    // const self = this;
+    // const isMillenial = function() {
+    //     console.log(self);
+    //     console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    //METHOD 2
+
+    const isMillenial = function () {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+
+  //     //PROBLEM OR BUG IN JAVASCRIPT
+  //     const isMillenial = function () {
+  //       console.log(this);
+  //       console.log(this.year >= 1981 && this.year <= 1996);
+  //     };
+  //     isMillenial();
+
+  //     //HOW TO SOLVE
+  //     //METHOD 1 (ku duhet te perdorim nje variabel ekstra ku zakonisht e quajme self )
+  //     //do shtojme const self = this; (pasi ketu jemi jashte nga funksioni isMillennial)
+  //
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`); //pra nese e leme me funksionin arrow ne output do kemi : Hey undefined
+    //   greet: function () {
+    //     console.log(this);
+    //     console.log(`Hey ${this.firstName}`); // nese do kishim kete lloj funksioni ath outputi do jete: Hey Jonas
+  },
+};
+jonas.greet();
+jonas.calcAge();
+
+
+//arguments keyword
+
+const addExpr = function (a, b) => {
+    console.log(arguments);
+    return a + b;
+};
+
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+    console.log(arguments);
+    return a + b;
+};
+addArrow(2, 5, 8);
+*/
+
+/*------Primitives vs. Objects ------------*/
+/*let age = 26;
+let oldAge = age;
+age = 27;
+console.log(age);
+console.log(oldAge);
+
+//create an object me
+const me = {
+  name: 'Genta',
+  age: 26,
+};
+
+//create an object for friends equals me
+const friend = me;
+friend.age = 28; //ne kete rast dhe age of me is 28
+console.log('Friend:', friend);
+console.log('Me', me);
+*/
+//Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+// create an object called jessica
+//Reference types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+//create an object per jessica te martuar
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage:', marriedJessica);
+
+//Coping objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+const jessicaCopy = Object.assign({}, jessica2); //berja e merge te dy objekteve
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -11,7 +11,6 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
   openingHours: {
     thu: {
       open: 12,
@@ -26,4 +25,124 @@ const restaurant = {
       close: 24,
     },
   },
+  //do krijojme nje method ku si parametra do kemi index e startmenu dhe nje index te main menu
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  /*
+  // tani bejme destructuing od orderDelivery
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    // pra vendos te gjitha property of orderDelivery
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered  to ${address} at ${time}`
+    );
+  },
+*/
+  // ose e bejme
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered  to ${address} at ${time}`
+    );
+  },
+
+  // orderDelivery: function (obj) {
+  //   //call argument obj
+  //   console.log(obj);
+  // },
 };
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'Via del Sole, 21',
+  starterIndex: 1,
+});
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+//Default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b } = obj);
+console.log(a, b);
+
+//Nested objects
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close);
+//////////////////////////////////////////////
+/*
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary); //Outputi: Italian, Vegetarian
+
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+[main, secondary] = [secondary, main];
+console.log(main, secondary); //Outputi: Vegetarian, Italian
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse); //Outputi: Garlic Bread Pizza
+//console.log(restaurant.order(2, 0));
+
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j); //output: 2, [5, 6]
+const [i, , [j, k]] = nested;
+console.log(i, j, k); //output: 2, 5, 6
+
+//Default values
+const [p, q, r] = [8, 9];
+console.log(p, q, r); //output: 8, 9, undefined
+
+/*
+ 
+};
+*/
+
+// join 2 arrays
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arrNew = [...arr1, ...arr2];
+console.log(arrNew);
+
+//Iterables:  arrays, strings, maps, sets. NOT objects
+const str = 'Genta';
+const letters = [...str, ' ', 'H.'];
+console.log(letters); // ben te mundur ndarjen e emrit me germa . pra outputi: ['G', 'e', 'n', 't', 'a', ' ', 'H.']
+console.log(...str);
